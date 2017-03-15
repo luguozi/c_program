090211ad002658642e79d14e3e220d2df208d4e9
luguozi <88@luguozi.com>
2017-03-15 17:48:30 +0800

    add ctype_lmm.h

--- c_lib/ctype.h/isupper.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int
-isupper(char c){
-return (c>='A'&&c<='Z') ? 1:0;
-}
---
+++ c_lib/ctype_lmm.h
@@ -0,0 +1,13 @@
+//this is writed by lmmys 
+#define isalpha(c) ((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+#define isblank(c) ((c==' '||c=='\t')?1:0)
+#define iscntrl(c) (((c>=0&&c<=31)||c==63)?1:0)
+#define isdigit(c) ((c>='0'&&c<='9')?1:0)
+#define islower(c) ((c>='a'&&c<='z')?1:0)
+#define isprint(c)
+#define ispunct(c)
+#define isupper(c) ((c>='A'&&c<='Z')?1:0)
+#define isspace(c)
+#define isxdigit(c)
+
+
